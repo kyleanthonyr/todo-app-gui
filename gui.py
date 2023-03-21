095e13e8887d0e57c00e725da73ed0dce27011ff
Kyle <128398210+kyleanthonyr@users.noreply.github.com>
2023-03-21 12:45:16 -0500

    Implemented 'Clear' button interaction and changed theme

--- gui.py
+++ gui.py
@@ -1,9 +1,14 @@
+import time
+
 import PySimpleGUI as sg
 
 import functions
 
+sg.theme('LightBlue')
+
 # LABEL WIDGETS
 label = sg.Text("Type in a to-do: ")
+clock = sg.Text('', key='clock')
 
 # INPUT WIDGETS
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
@@ -11,24 +16,24 @@ list_box = sg.Listbox(values=functions.get_todos(), key="todos",
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 
 # BUTTONS
-exit_btn = sg.Button('Exit')
-edit_button = sg.Button("Edit")
-add_button = sg.Button("Add")
-complete_btn = sg.Button("Complete")
+exit_btn = sg.Button('Exit', size=6)
+edit_button = sg.Button("Edit", size=6)
+add_button = sg.Button("Add", size=6)
+complete_btn = sg.Button("Complete", size=10)
+clear_btn = sg.Button("Clear", size=6)
 
 # MAIN GUI WINDOW
 window = sg.Window("My TO-DO App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_btn],
-                           [exit_btn]],
-                   font=('Helvetica', 20))
+                           [exit_btn, clear_btn]],
+                   font=('Helvetica', 18))
 
 while True:
-    event, values = window.read()
-    print(f'1. The event is {event}')
-    print(f'2. The values of the event is: {values}')
-    print(f'3. The to-do is {values["todos"]}')
+    event, values = window.read(timeout=200)
+    window['clock'].update(value=time.strftime('%b %d, %Y %M:%H:%S'))
     match event:
         # ADD to-do feature
         case 'Add':
@@ -40,25 +45,37 @@ while True:
 
         # EDIT Feature
         case 'Edit':
-            selected_todo = values['todos'][0]  # gets the to-do user wishes to edit
-            new_todo = values['todo']  # gets new to-do from user input
+            try:
+                selected_todo = values['todos'][0]  # gets the to-do user wishes to edit
+                new_todo = values['todo']  # gets new to-do from user input
 
-            todos = functions.get_todos()
-            index = todos.index(selected_todo)  # gives the index of the existing to-do the user selected
-            todos[index] = new_todo  # replaces old to-do with new one based on its index
-            functions.write_todos(todos)  # updates the to-do list with new todos
-            window["todos"].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(selected_todo)  # gives the index of the existing to-do the user selected
+                todos[index] = new_todo  # replaces old to-do with new one based on its index
+                functions.write_todos(todos)  # updates the to-do list with new todos
+                window["todos"].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 16), title="No Items Selected")
 
         case 'Complete':
-            selected_todo = values['todos'][0]  # gets the to-do user wishes to edit
-            todos = functions.get_todos()
+            try:
+                selected_todo = values['todos'][0]  # gets the to-do user wishes to edit
+                todos = functions.get_todos()
 
-            todos.remove(selected_todo)
+                todos.remove(selected_todo)
+                functions.write_todos(todos)
+
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica', 16), title="No Items Selected")
+
+        case 'Clear':
+            todos = functions.get_todos()
+            todos.clear()
             functions.write_todos(todos)
-            
             window['todos'].update(values=todos)
             window['todo'].update(value='')
-
         # REFRESH to-do in InputBox
         case 'todos':
             # when you have a list box event, point to the input box and update with selected to-do from listbox
